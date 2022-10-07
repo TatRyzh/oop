df412a3a4cd56f0cd0574ccecfe8dd96a564c693
TatRyzh <tanya_ryzhova@yahoo.com>
2022-10-07 16:36:24 -0700

    Cats and Dogs and Frogs

--- main.py
+++ main.py
@@ -5,24 +5,47 @@
 #5.2 Создайте репозиторий на Github и отправьте файл с домашним заданием в этот удаленный репозиторий
 
 
-class Person:
-    some_num = 123
-    def __init__(self, name, lastname, placeofbirth, yearofbirth):
-        self.name = name                                        # <-- атрибуты класса
-        self.lastname = lastname
-        self.placeofbirth = placeofbirth
-        self.yearofbirth = yearofbirth
-    def printInfo(self, n):
-        for i in range(n):
-            print(f'Name = {self.name},Last Name = {self.lastname},POB = {self.placeofbirth}')
-    def get_age(self, current_year):
-        return current_year - self.yearofbirth
+class Animal:
+    #some_num = 123              # атрибуты класса (статические атрибуты), все обекты класса имеют доступ
 
-p1 = Person('I.S.', 'Bach', 'Germany', 1685)
-p2 = Person('W.A.', 'Mozart', 'Austria', 1756)
-p3 = Person('Sergey','Rachmaninoff', 'Russia', 1873)
+    def __init__(self, name):
+        self.name = name                                        # <-- атрибуты об'ектов класса
+
+    def eat(self):
+        print(f"{self.name} is eating")
+
+class Dog(Animal):
+    # переопределим конструктор для собаки - персональный
+    def __init__(self, name, breed):
+        #Animal.__init__(self, name, breed)
+        super().__init__(name)          # указание на родительский конструктор
+        self.breed = breed
+
+        print(f"A dog {self.name} of breed {self.breed} is created")
+    def bark(self):
+        print(f'{self.name} is barking')
 
 
-p3.printInfo(1)
+class Cat(Animal):
+    def meow(self):
+        print(f'{self.name} is meowing')
 
-#Person.printInfo(p1)
+class Frog(Animal):
+
+    def eat(self):
+        print(f'{self.name} is eating')
+
+
+d=Dog ('Druzhok', 'Terrier')
+c = Cat ('Murka')
+f= Frog ('Kwakwa')
+
+d.bark()
+d.breed         # почему метод breed без скобок?
+d.eat()
+c.meow()
+c.eat()
+f.eat()
+
+
+
